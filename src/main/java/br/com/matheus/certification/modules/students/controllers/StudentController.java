e0b8536fc72252970ddbe1db7fbb69ad81302d67
Matheus Mello <matheusgmello1@gmail.com>
2024-02-11 19:32:51 -0300

    feat: use case to check if the user has already taken the test

--- src/main/java/br/com/matheus/certification/modules/students/controllers/StudentController.java
+++ src/main/java/br/com/matheus/certification/modules/students/controllers/StudentController.java
@@ -1,19 +1,28 @@
 package br.com.matheus.certification.modules.students.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.matheus.certification.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.matheus.certification.modules.students.useCases.VerifyHasCertificationUseCase;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+  @Autowired
+  private VerifyHasCertificationUseCase verifyHasCertificationUseCase;
   
   @PostMapping("/verifyIfHasCertification")
   public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
-    System.out.println(verifyHasCertificationDTO);
+
+    var result = this.verifyHasCertificationUseCase.execute(verifyHasCertificationDTO);
+    if(result){
+      return "Usuário já fez a avaliação";
+    }
     return "Usuário pode fazer a avaliação";
   }
 
