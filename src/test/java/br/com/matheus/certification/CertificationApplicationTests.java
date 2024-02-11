d6a6e49a9c62bcd8e3cdd8e64cd3dcfad519fcf0
Matheus Mello <matheusgmello1@gmail.com>
2024-02-11 18:47:24 -0300

    feat: first commit

---
+++ src/test/java/br/com/matheus/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
