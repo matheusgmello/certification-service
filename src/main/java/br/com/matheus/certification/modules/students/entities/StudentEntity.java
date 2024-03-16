847d8da39194905b99d0801e310a599ee1a03507
Matheus Mello <matheusgmello1@gmail.com>
2024-03-16 12:14:24 -0300

    feat: add mapping and relationship between entities

--- src/main/java/br/com/matheus/certification/modules/students/entities/StudentEntity.java
+++ src/main/java/br/com/matheus/certification/modules/students/entities/StudentEntity.java
@@ -1,8 +1,13 @@
 package br.com.matheus.certification.modules.students.entities;
-
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
   
+  @Id
+  @GeneratedValue(strategy =  GenerationType.UUID)
   private UUID id;
-  private String emal;
+  
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @OneToMany(mappedBy = "studentEntity")
   private List<CertificationStudentEntity> certificationStudentEntity;
 }
